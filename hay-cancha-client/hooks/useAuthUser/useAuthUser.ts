4cc13cee07f322f397e5045ba82399677337b91e
EduAZ86 <eduardoayaviri@gmail.com>
2024-11-27 00:47:51 -0300

    feature: useSyncWithBack

--- hay-cancha-client/hooks/useAuthUser/useAuthUser.ts
+++ hay-cancha-client/hooks/useAuthUser/useAuthUser.ts
@@ -3,17 +3,46 @@ import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { IcreateUserParams } from "./types";
+import axios from "axios";
+
+const BACKEND_URL_BASE = "http://localhost:3000";
 
 export const useAuthUser = () => {
+
+    // sincroniza el backend con firebase enviando un token con la sesion
+    const useSyncWithBackend = () => {
+        return useMutation({
+            mutationKey: ["syncBackend"],
+            mutationFn: async ({ path, token }: { path: string; token: string }) => {
+                await axios.post(`https://tu-backend.com/api${path}`, { token });
+            },
+            onError(error) {
+                console.error("Error al sincronizar con el backend:", error);
+            },
+        });
+    };
+
     const useLoginUser = () => {
+
         const router = useRouter();
+        const syncWithBackend = useSyncWithBackend();
+
         const mutation = useMutation({
             mutationKey: ["loginUser"],
             mutationFn: async ({ email, password }: { email: string; password: string }) => {
-                return signInWithEmailAndPassword(authFirebase, email, password);
+                const userCredentials = await signInWithEmailAndPassword(authFirebase, email, password);
+                return userCredentials;
             },
-            onSuccess: (data, variables) => {
-                router.push("/");
+            onSuccess: async (userCredential) => {
+                const idToken = await userCredential.user.getIdToken(); // Obtener el token de Firebase
+                syncWithBackend.mutate(
+                    { path: "/auth/login", token: idToken },
+                    {
+                        onSuccess: () => {
+                            router.push("./");
+                        },
+                    }
+                );
             },
             onError(error, variables) {
                 console.log("error", error);
@@ -23,18 +52,29 @@ export const useAuthUser = () => {
     }
 
     const useCreateUser = () => {
+
         const router = useRouter();
+        const syncWithBackend = useSyncWithBackend();
+
         const mutation = useMutation({
             mutationKey: ["createUser"],
             mutationFn: ({ email, password, name }: IcreateUserParams) => {
                 return createUserWithEmailAndPassword(authFirebase, email, password)
             },
-            onSuccess: async (data, variables) => {
+            onSuccess: async (userCredential, variables) => {
                 const { name } = variables
-                await updateProfile(data.user, {
+                await updateProfile(userCredential.user, {
                     displayName: name
                 })
-                router.push("/auth/login")
+                const idToken = await userCredential.user.getIdToken(); // Obtener el token de Firebase
+                syncWithBackend.mutate(
+                    { path: "/auth/register", token: idToken },
+                    {
+                        onSuccess: () => {
+                            router.push("/auth/login");
+                        },
+                    }
+                );       
             },
             onError(error, variables) {
                 console.log("error", error)
