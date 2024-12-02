0b70d6bfada55bc00ba2852f79979d9af678ea38
Adri-ESM <anggivmorales@gmail.com>
2024-12-02 11:38:30 -0300

    Eliminando hay-cancha-server de client y actualizando .gitignore

--- hay-cancha-server/src/firebase.ts
+++
@@ -1,9 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as serviceAccount from '../firebase-key.json';
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-});
-
-const db = admin.firestore();
-export { db };
