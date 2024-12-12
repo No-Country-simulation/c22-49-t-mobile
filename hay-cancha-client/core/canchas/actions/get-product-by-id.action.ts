56a50d160f2a7e8a0097c4ea5a896363fd037c47
Mauri <mauri.monzon91@gmail.com>
2024-12-12 14:49:55 -0300

    action cancha por id

--- hay-cancha-client/core/canchas/actions/get-product-by-id.action.ts
+++ hay-cancha-client/core/canchas/actions/get-product-by-id.action.ts
@@ -1,29 +1,12 @@
-import { API_URL, productsApi } from "@/core/api/productsApi";
-import { Gender, Product } from "../interfaces/cancha.interface";
+import { productsApi } from "@/core/api/productsApi";
+import { Cancha } from "../interfaces/cancha.interface";
 
-const emptyProduct: Product = {
-  id: "",
-  title: "Nuevo producto",
-  description: "",
-  price: 0,
-  images: [],
-  slug: "",
-  gender: Gender.Men,
-  sizes: [],
-  stock: 0,
-  tags: [],
-};
 
 export const getProductById = async (id: string) => {
-  if (id === "new") return emptyProduct;
-  
-  try {
-    const { data } = await productsApi.get<Product>(`/products/${id}`);
 
-    return {
-      ...data,
-      images: data.images.map((image) => `${API_URL}/files/product/${image}`),
-    };
+  try {
+    const { data } = await productsApi.get<Cancha>(`/cancha/${id}`);
+    return data
   } catch (error) {
     throw new Error(`No se encontro el producto de id: ${id}`);
   }
--- hay-cancha-client/core/canchas/interfaces/cancha.interface.ts
+++ hay-cancha-client/core/canchas/interfaces/cancha.interface.ts
@@ -1,14 +1,42 @@
 export interface Cancha {
-  name: string;
-  location: string;
-  type: string;
+  _id:            string;
+  name:           string;
+  location:       Location;
+  type:           string;
   price_per_hour: number;
-  opening_hours: string;
-  closing_time: string;
-  state: string;
-  images: string;
-  _id: string;
-  createdAt: Date;
-  updatedAt: Date;
-  __v: number;
+  opening_hours:  OpeningHours;
+  closing_time:   ClosingTime;
+  state:          State;
+  images:         string[];
+  address:        Address;
+  createdAt:      Date;
+  updatedAt:      Date;
+  __v:            number;
+}
+
+export interface Address {
+  streetName:   StreetName;
+  streetNumber: number;
+  _id:          string;
+}
+
+export enum StreetName {
+  AVSiempreViva = "Av. Siempre Viva",
+}
+
+export enum ClosingTime {
+  The2200 = "22:00",
+}
+
+export enum Location {
+  Belgrano = "Belgrano",
+  Palermo = "Palermo",
+}
+
+export enum OpeningHours {
+  The0800 = "08:00",
+}
+
+export enum State {
+  Disponible = "disponible",
 }
